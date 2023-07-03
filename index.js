df4d7d073ea308cbf7eb5c09c0c72686592418b0
HIMANSHU RAI <himanshuuu.2001@gmail.com>
2023-07-03 17:50:26 +0530

    linked to form

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let email = document.getElementById("email");
 let password = document.getElementById("password");
 
-var emailList = ["edible@mail.com"];
-var passwordList = ["1234"]
+var emailList = ["edible@mail.com","bbrl@mail.com"];
+var passwordList = ["1234","1234"];
 
 function validate(){
     if(emailList.includes(email.value) && passwordList.includes(password.value)){
-        window.location = "https://forms.gle/wGHJuW3ugQejeG7a6";
+        window.location.replace("customer.html");
     }
     else{
         email.style.border="1px solid red";
